92f2b9e31de66a20fabed22e65298d12cf06a1ac
chillner <86615601+chillner@users.noreply.github.com>
2022-06-22 16:37:29 +0200

    Update sect.5.1_5.2.R
    Added more comments to simplify usage and understanding

--- sect.5.1_5.2.R
+++ sect.5.1_5.2.R
@@ -28,16 +28,19 @@ beta <- 0.2; alpha = 0.025
 #grid for pi12:
 pi12 <- seq(0, 1, 0.01)
 factor_fwer <- factor_pwer <- numeric(length(pi12))
-#plug in corpi12_1(pi12[i]) for corr to get the one treatment case (T1 = T2) in Section 5.2
-#plug in corpi12_2(pi12[i]) for corr to get the two treatment case (T1 != T2) in Section 5.2
-#plug in 0 for corr to get the independent case in Section 5.1
+
+#Calculate sample size factors for each value in pi12
 for(i in 1:length(pi12)){
   factor_fwer[i] <- (qnorm(1-beta) + mtcritfwer(alpha=alpha, pi12=pi12[i], corr = corpi12_2(pi12[i])))^2/(qnorm(1-beta)+qnorm(1-alpha))^2
   factor_pwer[i] <- (qnorm(1-beta) + mtcritpwer(alpha=alpha, pi12=pi12[i], corr = corpi12_2(pi12[i])))^2/(qnorm(1-beta)+qnorm(1-alpha))^2
 }
-
+#Plots
 plot(pi12, factor_fwer, xlab = expression(pi[12]), ylim = c(1, 1.25), type = "l",
 	ylab = "Factor of sample size increase for PWER/FWER", col = "red")
 lines(pi12, factor_pwer, col = "blue")
 text(x=0.1, y=1.215, labels = "FWER", col = "red")
 text(x=0.1, y=1.045, labels = "PWER", col = "blue")
+
+#plug in corpi12_1(pi12[i]) in the for-loop above for corr in mtcritpwer/fwer to get the one treatment case (T1 = T2) in Section 5.2
+#plug in corpi12_2(pi12[i]) in the for-loop above for corr in mtcritpwer/fwer to get the two treatment case (T1 != T2) in Section 5.2
+#plug in 0 in the for-loop above for corr in mtcritpwer/fwer to get the independent case in Section 5.1
